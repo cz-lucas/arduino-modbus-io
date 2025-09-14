d963deb682a4175cc7d16c59ac09dc821c06fab0
Lucas <46531925+cz-lucas@users.noreply.github.com>
2025-09-14 12:36:57 +0200

    Added modbus

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,42 @@
 #include <Arduino.h>
 #include <build_info.h>  // This will be auto-generated
-#include "pins.h"
+#include "config.h"
+
+#include <SoftwareSerial.h>
+#include <ModbusRTUSlave.h>
+
+
+SoftwareSerial swSerial (MODBUS_RX, MODBUS_TX);
+#define MODBUS_SERIAL swSerial
+#define DEBUG_SERIAL Serial
+ModbusRTUSlave modbus(MODBUS_SERIAL, MAX485_DERE);
+
+const uint8_t numInputRegisters = 2;
+uint16_t inputRegisters[numInputRegisters];
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println("Arduino modbus node");
-  Serial.println(String("Commit: ") + GIT_COMMIT_HASH);
-  Serial.println(String("Buildtime: ") + BUILD_TIMESTAMP);
-  Serial.println("####################################");
+  DEBUG_SERIAL.begin(115200);
+  DEBUG_SERIAL.println("Arduino modbus node");
+  DEBUG_SERIAL.println(String("Commit: ") + GIT_COMMIT_HASH);
+  DEBUG_SERIAL.println(String("Buildtime: ") + BUILD_TIMESTAMP);
+  DEBUG_SERIAL.println("####################################");
+
+  DEBUG_SERIAL.println("[SETUP] Initializing modbus");
+  //modbus.configureCoils(coils, numCoils);
+  //modbus.configureDiscreteInputs(discreteInputs, numDiscreteInputs);
+  //modbus.configureHoldingRegisters(holdingRegisters, numHoldingRegisters);
+  modbus.configureInputRegisters(inputRegisters, numInputRegisters);
+  MODBUS_SERIAL.begin(9600);
+  modbus.begin(MODBUS_SLAVE_ID, MODBUS_BAUD, MODBUS_CONFIG);
 
   pinMode(LEDPIN, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(LEDPIN, !digitalRead(LEDPIN));
-  delay(250);
+  inputRegisters[0] = random(0,200);
+  inputRegisters[1] = 200;
+  modbus.poll();
+  //digitalWrite(LEDPIN, !digitalRead(LEDPIN));
 }
--- src/pins.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-#define LEDPIN LED_BUILTIN
